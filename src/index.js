0608568795d2dabf7c76c17dcefa3bb9059e63b2
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-06 10:49:02 -0700

    added basic html data

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// fetch Get to all activities: http://localhost:3000/api/v1/activities
+// fetch Get to all users : http://localhost:3000/api/v1/users
+// fetch Get to all comments: http://localhost:3000/api/v1/comments
