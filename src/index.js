61790607e4ab62600e98479e07c61d1e966e39fa
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-06 16:54:32 -0700

    add sidebar with new activity form-not done yet

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@
 
 const activitiesURL = "http://localhost:3000/api/v1/activities";
 const mainTag = document.querySelector("main");
-const form = document.querySelector("form");
-const activitiesContainer = document.getElementById("activity-container");
+const loginForm = document.getElementById("login-form");
+const mySidebar = document.getElementById("mySidebar");
 
 // createLoginForm();
 listenToLoginForm();
 function listenToLoginForm() {
-  form.addEventListener("submit", function (event) {
+  loginForm.addEventListener("submit", function (event) {
     event.preventDefault();
     if (
       event.target.childNodes[0].value !== "" &&
       event.target.childNodes[1].value !== ""
     ) {
-      form.className = "hidden";
+      loginForm.className = "hidden";
       fetchActivities();
     } else {
       console.error("error");
@@ -43,6 +43,7 @@ function appendActivities(activity) {
     category,
     comments,
   } = activity;
+  const activitiesContainer = document.getElementById("activity-container");
   const div = document.createElement("div"); // card
   const h2 = document.createElement("h2"); // name
   const h4 = document.createElement("h4"); // category
@@ -52,17 +53,39 @@ function appendActivities(activity) {
   const ul = document.createElement("ul"); // comments
 
   div.className = "card";
+  div.style = "overflow:scroll";
   h2.textContent = activity.name;
   h4.textContent = activity.category;
   h3.textContent = `${activity.address}, ${activity.city}, ${activity.state}`;
   p.textContent = activity.description;
+  ul.innerHTML += createCommentList(activity.comments);
   img.src = activity.image;
   img.style.width = "100%";
 
-  div.append(h2, h3, img, h4, p);
+  div.append(h2, h3, img, h4, p, ul);
   activitiesContainer.append(div);
+}
 
-  debugger;
+function createCommentList(comments) {
+  return comments.map((comments) => `<li>${comments.content}</li>`).join(" ");
+}
+
+function openNav() {
+  mySidebar.style.width = "50%";
+  document.getElementById("main").style.marginLeft = "50%";
+  createNewActivity();
+}
+
+function closeNav() {
+  mySidebar.style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+function createNewActivity() {
+  const form = document.getElementById("new-Activity-Form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
 }
 
 // function createLoginForm() {
@@ -78,3 +101,39 @@ function appendActivities(activity) {
 
 //   form.append(nameInputTag, addressInputTag, btn);
 // }
+
+// function createNewActivity() {
+//     const form = document.createElement("form");
+//     const inputName = document.createElement("input");
+//     const inputCategory = document.createElement("input");
+//     const inputAdress = document.createElement("input");
+//     const inputCity = document.createElement("input");
+//     const inputState = document.createElement("input");
+//     const inputZip = document.createElement("input");
+//     const inputDescription = document.createElement("input");
+//     const inputImg = document.createElement("input");
+//     const btn = document.createElement("button");
+
+//     inputName.placeholder = "Activity Name";
+//     inputCategory.placeholder = "Category";
+//     inputAdress.placeholder = "Address";
+//     inputCity.placeholder = "City";
+//     inputState.placeholder = "State";
+//     inputZip.placeholder = "Zipcode";
+//     inputDescription.placeholder = "description";
+//     inputDescription.style.height = "200%";
+//     inputImg.placeholder = "Image URL";
+//     btn.textContent = "Add New Activity";
+
+//     form.append(
+//       inputName,
+//       inputCategory,
+//       inputAdress,
+//       inputCity,
+//       inputZip,
+//       inputDescription,
+//       inputImg,
+//       btn
+//     );
+//     mySidebar.append(form);
+//     }
