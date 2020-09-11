d12af210bea678e2a3668819c8c616ea9b9bc56e
Noa Rabin-Cohen <66642491+cohenoa33@users.noreply.github.com>
2020-09-11 09:44:47 -0700

    more css changes

--- src/users.js
+++ src/users.js
@@ -1,25 +1,26 @@
-const floatingPanel = document.querySelector('#floating-panel')
-const confirmBtn = document.querySelector('#submit');
-closeFloatingPanel()
+const floatingPanel = document.querySelector("#floating-panel");
+const confirmBtn = document.querySelector("#submit");
+closeFloatingPanel();
 
 function closeFloatingPanel() {
-  confirmBtn.addEventListener('click', function(e) {
-    const nameContainer = document.querySelector('#name')
+  confirmBtn.addEventListener("click", function (e) {
+    const nameContainer = document.querySelector("#name");
     const name = nameContainer.value;
-    
+
     if (name) {
-      const nav = document.querySelector('nav');
-      const greeting = document.createElement('button');
-      nav.append(greeting);
+      const usercontainer = document.querySelector(".helloUser");
+      const greeting = document.createElement("button");
+
+      usercontainer.append(greeting);
       greeting.textContent = `Hello ${name}!`;
-      nameContainer.style.display = 'none';
-      
-      greeting.addEventListener('click', function() {
-        greeting.remove()
-        nameContainer.style.display = 'inline';
+      nameContainer.style.display = "none";
+
+      greeting.className = "helloButton";
+
+      greeting.addEventListener("click", function () {
+        greeting.remove();
+        nameContainer.style.display = "inline";
       });
     }
   });
 }
-
-
